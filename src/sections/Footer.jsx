583e70917744bac9b64d82650517fe038d35980c
swalihvnr <swalihvnr@users.noreply.github.com>
2026-08-22 22:53:19 +0530

    Shift Upscrolled logo right

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -98,7 +98,7 @@ export default function Footer() {
                 aria-label={label}
                 target="_blank"
               >
-                {Icon ? <Icon size={20} /> : <img src="/assets/upscrolled.svg" alt={label} className="w-7 h-7 object-contain" style={{ objectPosition: 'center 30%' }} />}
+                {Icon ? <Icon size={20} /> : <img src="/assets/upscrolled.svg" alt={label} className="w-7 h-7 object-contain" style={{ objectPosition: '60% 30%' }} />}
               </a>
             ))}
           </div>
